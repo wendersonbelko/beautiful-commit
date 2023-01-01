c14e3c470037406e4574368bfb3802baea97754d
wenderson belko <contato@wendersonbelko.com>
2023-01-01 12:00:00 -0300

    Commit para o dia 0 - commit 1

--- run.sh
+++ run.sh
@@ -9,7 +9,8 @@ git init
 # Loop para percorrer os últimos 365 dias
 for (( i=0; i<365; i++ )); do
     # Calcula a data subtraindo 'i' dias da data atual
-    data=$(date -d "$ano_atual-12-31 -$i days" +%d)
+    data="$ano_atual-01-01 -$i days"
+    data_formatada=$(date -d "$data" +%Y-%m-%d)
     
     # Gera um número aleatório entre 0 e 1 (0 significa não commitar, 1 significa commitar)
     commit_aleatorio=$((RANDOM % 2))
@@ -17,7 +18,7 @@ for (( i=0; i<365; i++ )); do
     # Se o número gerado for 1, commitar
     if [ "$commit_aleatorio" -eq 1 ]; then
         # Exibe o dia no formato desejado
-        echo "dia $data"
+        echo "dia $i"
         
         # Gera um número aleatório entre 1 e 10 para determinar a quantidade de commits
         quantidade_commits=$((RANDOM % 10 + 1))
@@ -25,13 +26,10 @@ for (( i=0; i<365; i++ )); do
         # Loop para criar os commits
         for (( j=0; j<quantidade_commits; j++ )); do
             # Cria um arquivo com a mensagem do commit
-            echo "dia $data - commit $((j+1))" > commit_$data_$j.txt
+            echo "dia $i - commit $((j+1))" > commit_message.txt
             
-            # Adiciona o arquivo ao índice do Git
-            git add commit_$data_$j.txt
-            
-            # Realiza o commit com a mensagem formatada
-            git commit -m "Commit para o dia $data - commit $((j+1))"
+            # Adiciona o arquivo ao índice do Git e define a data do commit
+            env GIT_COMMITTER_DATE="$data_formatada 12:00:00 -$j seconds" git commit --date="$data_formatada 12:00:00 -$j seconds" -am "Commit para o dia $i - commit $((j+1))"
         done
     fi
 done
